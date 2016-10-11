b767576fa6bb5342417b5a668f925f8578c50eb3
yoannadp <yoanna_petkova@abv.bg>
2016-10-11 16:25:31 +0300

    --

--- header.php
+++ header.php
@@ -1,11 +1,3 @@
-<header>
-				<img id="first_frame" src="images/decor_1.png">
-				<div class="menu other">
-					<div>menu</div>
-					<div>gallery</div>
-					<div>la carmencita</div>
-					<div>contacts & schedule</div>
-						<img class="welcome" src="images/logo_blue.png">
-				</div>	
-				<h2>Hello</h2>
-			</header>
+<?php
+echo "string";
+?>
