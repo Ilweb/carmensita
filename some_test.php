d3e596e18e13dabedbcca89d408e53832ddb4e65
yoannadp <yoanna_petkova@abv.bg>
2016-10-11 16:27:03 +0300

    -

---
+++ some_test.php
@@ -0,0 +1,15 @@
+<? php 
+
+<header>
+	<img id="first_frame" src="images/decor_1.png">
+	<div class="menu other">
+		<div>menu</div>
+		<div>gallery</div>
+		<div>la carmencita</div>
+		<div>contacts & schedule</div>
+			<img class="welcome" src="images/logo_blue.png">
+			echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
+	</div>	
+	<h2>Hello</h2>
+</header>
+?>
