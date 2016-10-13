476a132066396a21b7e5287e288b63b12e0d2fe3
yoannadp <yoanna_petkova@abv.bg>
2016-10-13 10:52:44 +0300

    -

--- header.php
+++ header.php
@@ -1,3 +1,14 @@
-<?php
-echo "string";
-?>
+<header>
+				<div class="mobile_menu">MENU</div>
+					<div class="menu first-menu">
+						<nav class="main">
+							<ul class="mainNav">
+								<li><a href="#">menu</a></li>
+								<li><a href="#">galerry</a></li>
+								<li><a href="#">la carmencita</a></li>
+								<li><a href="#">contacts & schedule</a></li>
+							</ul>
+						</nav>
+					</div>	
+				<img id="first_frame" src="images/decor_1.png">
+			</header>
