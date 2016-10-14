b1776107cf118c1dad92e0f0dd06f39eb11726e8
yoannadp <yoanna_petkova@abv.bg>
2016-10-14 10:10:27 +0300

    -

--- header.php
+++ header.php
@@ -12,3 +12,38 @@
 					</div>	
 				<img id="first_frame" src="images/decor_1.png">
 			</header>
+
+			div class="newspaper"><p><span>“Going tapas”</span> has nothing to do with the size of your dish, it has to do with what is in your dish and how you eat it.<br/></p>
+		<p><span>“Going tapas”</span> means sharing; slow food; not eating everything at the same time and having different drinks depending on what you are eating.</p>
+	<p><span>“Going tapas”</span> means a unique gastronomic experience in which you are going to try much more than two courses and a couple of wines.</p>
+		</div>
+		<div class="welcome"><img src="images/leaf.png" /></div>
+		<h3>…And thats what we are offering, the ultimate tapas experience…</h3>
+		<div class="newspaper">Si puedes leer esto no te vamos a explicar lo que es irse de tapas. Seas Español, de algún país hermano o hallas pasado el tiempo suficiente como para aprender el idioma sabes de sobra a lo que nos referimos.
+	Lo único que nos gustaría aclararte es que no estas enfrente
+		 del típico negocio orientado a turistas y que por eso su calidad va a ser menor. NO!! Nos gusta tapear, nos gusta nuestra forma de entender la vida, nos gusta nuestro trabajo y en La Carmencita queremos transmitirlo de la mejor manera que sabemos.</div>
+
+		 newspaper {
+
+    -webkit-column-count: 2; /* Chrome, Safari, Opera */
+    -moz-column-count: 2; /* Firefox */
+    column-count: 2;
+    -webkit-column-gap: 67px; /* Chrome, Safari, Opera */
+    -moz-column-gap: 67px; /* Firefox */
+    column-gap: 67px;;
+    overflow: auto;
+}
+.content1{
+      margin-left: -19%;
+}
+
+#listMenu{
+  overflow: auto;
+  width: 75%;
+}
+
+.mainMenu{
+  max-width: 1050px;
+  margin: 0 auto;
+
+}
