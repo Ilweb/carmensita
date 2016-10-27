f6dec730f847e4ec9e078a49c3d9538c09e4a20b
yoannadp <yoanna_petkova@abv.bg>
2016-10-27 17:06:40 +0300

    -

--- scroll.js
+++
@@ -1,8 +0,0 @@
-
-$.Velocity
-    .RegisterEffect("scaleDown", {
-    	defaultDuration: 800,
-        calls: [ 
-            [ { opacity: '0', scale: '0.7', boxShadowBlur: '40px' }, 1]
-        ]
-    });
